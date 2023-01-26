6210aa572ae5d2637fb71253f4c311853611eb7a
Michal Slovik <Michal.Slovik@exxeta.sk>
2023-01-26 12:54:41 +0100

    updates

---
+++ 04_time_and_types/02/main.py
@@ -0,0 +1,9 @@
+def main():
+    input_value = int(input("Enter a number: "))
+
+    print("Decimal: ", input_value)
+    print("Binary: ", bin(input_value))
+    print("Hexadecimal: ", hex(input_value))
+
+if __name__ == "__main__":
+    main()
